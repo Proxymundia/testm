3aa0549c9a620c658fc5090f8aa420ce4fffcb92
Gendozzzz <sunflowermaina@gmail.com>
2024-09-11 16:14:55 +0300

    Betty and navigation fixed

--- src/input.c
+++ src/input.c
@@ -21,47 +21,59 @@ void rotateCamera(double angle)
 /**
  * handleInput - actions on directions
  */
-void handleInput(void) 
+void handleInput(void)
 {
 	const Uint8 *state = SDL_GetKeyboardState(NULL);
-	double moveSpeed = 0.05;  // Speed of movement
-    double rotSpeed = 0.03;   // Speed of rotation
-    // Move forward (W)
-    if (state[SDL_SCANCODE_W]) {
+	double moveSpeed = 0.05;  /*Speed of movement*/
+	double rotSpeed = 0.03;   /*Speed of rotation*/
+	/*Move forward (W)*/
+	if (state[SDL_SCANCODE_W])
+	{
 		double newX = posX + dirX * moveSpeed;
 		double newY = posY + dirY * moveSpeed;
 
-        // Check for collision with walls
-        if (map[(int)newX][(int)posY] == 0) posX = newX;
-        if (map[(int)posX][(int)newY] == 0) posY = newY;
+		/*Check for collision with walls*/
+		if (map[(int)newX][(int)posY] == 0)
+			posX = newX;
+		if (map[(int)posX][(int)newY] == 0)
+			posY = newY;
 	}
 
-	// Move backward (S)
-	if (state[SDL_SCANCODE_S]) {
+	/*Move backward (S)*/
+	if (state[SDL_SCANCODE_S])
+	{
 		double newX = posX - dirX * moveSpeed;
-        double newY = posY - dirY * moveSpeed;
+		double newY = posY - dirY * moveSpeed;
 
-        // Check for collision with walls
-        if (map[(int)newX][(int)posY] == 0) posX = newX;
-        if (map[(int)posX][(int)newY] == 0) posY = newY;
+		/*Check for collision with walls*/
+		if (map[(int)newX][(int)posY] == 0)
+			posX = newX;
+		if (map[(int)posX][(int)newY] == 0)
+			posY = newY;
 	}
 
-	// Move left (A)
-	if (state[SDL_SCANCODE_A]) {
+	/*Move left (A)*/
+	if (state[SDL_SCANCODE_A])
+	{
 		double oldDirX = dirX;
+
 		dirX = dirX * cos(rotSpeed) - dirY * sin(rotSpeed);
 		dirY = oldDirX * sin(rotSpeed) + dirY * cos(rotSpeed);
 		double oldPlaneX = planeX;
+
 		planeX = planeX * cos(rotSpeed) - planeY * sin(rotSpeed);
 		planeY = oldPlaneX * sin(rotSpeed) + planeY * cos(rotSpeed);
 	}
 
-	// Move right (D)
-	if (state[SDL_SCANCODE_D]) {
+	/*Move right (D)*/
+	if (state[SDL_SCANCODE_D])
+	{
 		double oldDirX = dirX;
+
 		dirX = dirX * cos(-rotSpeed) - dirY * sin(-rotSpeed);
 		dirY = oldDirX * sin(-rotSpeed) + dirY * cos(-rotSpeed);
 		double oldPlaneX = planeX;
+
 		planeX = planeX * cos(-rotSpeed) - planeY * sin(-rotSpeed);
 		planeY = oldPlaneX * sin(-rotSpeed) + planeY * cos(-rotSpeed);
 	}
